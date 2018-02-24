c593a9abed44e172625612d485a6c1a0b1bed73d
Ilias <ideros21@gmail.com>
2018-02-24 16:02:54 -0500

    connect to firebase

---
+++ src/fire.js
@@ -0,0 +1,13 @@
+// From firebase's "Add Firebase to your web app"
+import firebase from 'firebase'
+
+let config = {
+  apiKey: "AIzaSyBHpScqjoYoRbR9qQpyo02D_r_2oxaIloU",
+  authDomain: "virtual-chips.firebaseapp.com",
+  databaseURL: "https://virtual-chips.firebaseio.com",
+  projectId: "virtual-chips",
+  storageBucket: "virtual-chips.appspot.com",
+  messagingSenderId: "301476727106"
+}
+
+export default firebase.initializeApp(config)
