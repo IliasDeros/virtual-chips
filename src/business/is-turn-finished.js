6437225382ebbfccc448f6ee5b098779eec2af7d
Ilias Deros <ideros21@gmail.com>
2018-05-23 01:08:45 +0000

    Call a bet goes to next turn

--- src/business/is-turn-finished.js
+++ src/business/is-turn-finished.js
@@ -12,11 +12,11 @@ export default function isTurnFinished(table){
         playerBets = players
           .filter(p => ![State.ALL_IN, State.FOLDED].includes(p.state))
           .map(p => p.chips.bet),
-        
+
         allSameBet = !!playerBets.reduce((a, b) => a === b ? a : NaN),
         allDone = players.every(({ state }) =>
-          [State.ALL_IN, State.CALLED, State.CHECKED, State.FOLDED].includes(state)
+          [State.ALL_IN, State.BET, State.CALLED, State.CHECKED, State.FOLDED].includes(state)
         )
-  
+
   return players.length >= 2 && allSameBet && allDone
 }
--- src/business/is-turn-finished.test.js
+++ src/business/is-turn-finished.test.js
@@ -3,18 +3,25 @@ import State from '../constants/state'
 
 describe('isTurnFinished', () => {
     let table
-    
+
     describe('truthy', () => {
         beforeEach(() => { table = {} })
         afterEach(() => expect(isTurnFinished(table)).toBeTruthy())
-        
+
+        it('should be finished when player calls the highest best', () => {
+            table.player = {
+                0: { chips: { bet: 500 }, state: State.BET },
+                1: { chips: { bet: 500 }, state: State.CALLED }
+            }
+        })
+
         it('should be finished when all players called', () => {
             table.player = {
                 0: { chips: { bet: 500 }, state: State.CALLED },
                 1: { chips: { bet: 500 }, state: State.CALLED }
             }
         })
-        
+
         it('should be finished when all players called or folded', () => {
             table.player = {
                 0: { chips: { bet: 500 }, state: State.CALLED },
@@ -22,7 +29,7 @@ describe('isTurnFinished', () => {
                 2: { chips: { bet: 300 }, state: State.FOLDED }
             }
         })
-        
+
         it('should be finished when all players called or folded or all-in', () => {
             table.player = {
                 0: { chips: { bet: 500 }, state: State.CALLED },
@@ -31,7 +38,7 @@ describe('isTurnFinished', () => {
                 3: { chips: { bet: 100 }, state: State.ALL_IN }
             }
         })
-        
+
         it('should be finished when big blind is checked', () => {
             table.player = {
                 0: { chips: { bet: 200 }, state: State.CHECKED },
@@ -39,18 +46,18 @@ describe('isTurnFinished', () => {
             }
         })
     })
-    
+
     describe('falsy', () => {
         beforeEach(() => { table = {} })
         afterEach(() => expect(isTurnFinished(table)).toBeFalsy())
-        
+
         it('should not be finished when not all players called', () => {
             table.player = {
-                0: { chips: { bet: 500 }, state: State.CALLED },
+                0: { chips: { bet: 300 }, state: State.CALLED },
                 1: { chips: { bet: 500 }, state: State.BET }
             }
         })
-        
+
         it('should not be finished when all players called different bets', () => {
             table.player = {
                 0: { chips: { bet: 500 }, state: State.CALLED },
@@ -58,7 +65,7 @@ describe('isTurnFinished', () => {
                 2: { chips: { bet: 500 }, state: State.CALLED }
             }
         })
-        
+
         it('should not be finished when one player is checked', () => {
             table.player = {
                 0: { chips: { bet: 500 }, state: State.CHECKED }
