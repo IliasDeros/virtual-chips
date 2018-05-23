1751994708eb74631ccd9d98a1fe593f9a7781e2
Ilias Deros <ideros21@gmail.com>
2018-05-23 23:29:06 +0000

    add token files

---
+++ src/business/get-token.js
@@ -0,0 +1,43 @@
+import Token from '../constants/token'
+
+/*
+* compute the player's token associated to his position and the table round
+* - In heads-up play with two blinds, the small blind is on the dealer
+* - Order is :
+*     1. Dealer
+*     2. Small Blind
+*     3. Big Blind
+*/
+export default function getToken(table, player){
+  let token,
+      playerIds = Object.keys(table.player)
+  const dealerIndex = table.round % playerIds.length,
+        playerIndex = playerIds.indexOf(player.id)
+
+  if (isHeadsUp(table)){
+    token = playerIndex === dealerIndex ? Token.DEALER_SMALL : Token.BIG_BLIND
+  } else {
+    const smallBlindIndex = (dealerIndex + 1) % playerIds.length,
+          bigBlindIndex = (dealerIndex + 2) % playerIds.length
+
+    switch (playerIndex){
+      case dealerIndex:
+        token = Token.DEALER
+        break
+      case smallBlindIndex:
+        token = Token.SMALL_BLIND
+        break
+      case bigBlindIndex:
+        token = Token.BIG_BLIND
+        break
+      default:
+        token = null
+    }
+  }
+
+  return token
+}
+
+function isHeadsUp(table){
+  return Object.keys(table.player).length <= 2
+}
---
+++ src/constants/token.js
@@ -0,0 +1,7 @@
+// player token
+export default {
+  DEALER_SMALL: 'dealer & small blind',
+  DEALER: 'dealer',
+  SMALL_BLIND: 'small blind',
+  BIG_BLIND: 'big blind',
+}
