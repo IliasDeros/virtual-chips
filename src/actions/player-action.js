1a4abfd2b5a869d09c2f162ac71ff8336936c174
Ilias Deros <IliasDeros@users.noreply.github.com>
2022-06-05 00:50:55 +0000

    Add fold action

--- src/actions/player-action.js
+++ src/actions/player-action.js
@@ -1,152 +1,156 @@
-import Fingerprint from 'fingerprintjs2'
+import Fingerprint from "fingerprintjs2";
 import { get, getDatabase, onValue, ref, set } from "firebase/database";
-import State from '../constants/state'
-import getToken from '../business/get-token'
+import State from "../constants/state";
+import getToken from "../business/get-token";
+import selectors from "reducers/selectors";
 
-function getFireRef(endpoint, { table, player }){
-  let url = `table/${table.id}/player/${player.id}/${endpoint}`
+function getFireRef(endpoint, state) {
+  const [me] = selectors.getPlayers(state);
+  let url = `table/${state.table.id}/player/${me.id}/${endpoint}`;
   const db = getDatabase();
-  return ref(db, url)
+  return ref(db, url);
 }
 
-export function loadPlayerId(){
+export function loadPlayerId() {
   return (dispatch, getState) => {
-    new Fingerprint().get(result =>
+    new Fingerprint().get((result) =>
       dispatch({
-          type: 'SET_PLAYER_ID',
-          payload: result
-        })
-    )
-  }
+        type: "SET_PLAYER_ID",
+        payload: result,
+      })
+    );
+  };
 }
 
-export function loadPlayerName(){
+export function loadPlayerName() {
   return (dispatch, getState) => {
-    const state = getState()
+    const state = getState();
 
-    onValue(getFireRef('name', state), async snapshot => {
-      let playerName = snapshot.val()
+    onValue(getFireRef("name", state), async (snapshot) => {
+      let playerName = snapshot.val();
 
-      if (playerName){
+      if (playerName) {
         dispatch({
-          type: 'SET_PLAYER_NAME',
-          payload: playerName
-        })
+          type: "SET_PLAYER_NAME",
+          payload: playerName,
+        });
       } else {
         // generate a random player name
-        let res = await fetch('https://cors-anywhere.herokuapp.com/http://namey.muffinlabs.com/name.json')
-        const [name] = await res.json()
+        let res = await fetch(
+          "https://cors-anywhere.herokuapp.com/http://namey.muffinlabs.com/name.json"
+        );
+        const [name] = await res.json();
 
-        set(getFireRef('name', state), name)
+        set(getFireRef("name", state), name);
       }
-    })
-  }
+    });
+  };
 }
 
-export function loadPlayerState(){
+export function loadPlayerState() {
   return (dispatch, getState) => {
-    const state = getState()
+    const state = getState();
 
-    onValue(getFireRef('state', state), snapshot => {
-      let playerState = snapshot.val()
+    onValue(getFireRef("state", state), (snapshot) => {
+      let playerState = snapshot.val();
 
-      if (playerState){
+      if (playerState) {
         dispatch({
-          type: 'SET_PLAYER_STATE',
-          payload: playerState
-        })
+          type: "SET_PLAYER_STATE",
+          payload: playerState,
+        });
       } else {
-        set(getFireRef('state', state), State.IDLE)
+        set(getFireRef("state", state), State.IDLE);
       }
-    })
-  }
+    });
+  };
 }
 
-export function loadPlayerToken(){
-  return (dispatch, getState, done = function(){}) => {
-    onRoundUpdate(getState().table, table => {
-      updateToken(table)
-      done()
-    })
+export function loadPlayerToken() {
+  return (dispatch, getState, done = function () {}) => {
+    onRoundUpdate(getState().table, (table) => {
+      updateToken(table);
+      done();
+    });
 
-    function updateToken(table){
+    function updateToken(table) {
       const state = getState(),
-            token = getToken(table, state.player)
+        token = getToken(table, state.player);
 
       dispatch({
-        type: 'SET_PLAYER_TOKEN',
-        payload: token
-      })
-      set(getFireRef('token', state), token)
+        type: "SET_PLAYER_TOKEN",
+        payload: token,
+      });
+      set(getFireRef("token", state), token);
     }
-  }
+  };
 }
 
-export function allIn(){
+export function allIn() {
   return (_, getState) => {
-    set(getFireRef('state', getState()), State.ALL_IN)
-  }
+    set(getFireRef("state", getState()), State.ALL_IN);
+  };
 }
 
-export function bet(){
+export function bet() {
   return (_, getState) => {
-    set(getFireRef('state', getState()), State.BET)
-  }
+    set(getFireRef("state", getState()), State.BET);
+  };
 }
 
-export function call(){
+export function call() {
   return (_, getState) => {
-    set(getFireRef('state', getState()), State.CALLED)
-  }
+    set(getFireRef("state", getState()), State.CALLED);
+  };
 }
 
-export function check(){
+export function check() {
   return (_, getState) => {
-    set(getFireRef('state', getState()), State.CHECKED)
-  }
+    set(getFireRef("state", getState()), State.CHECKED);
+  };
 }
 
-export function fold(){
+export function fold() {
   return (_, getState) => {
-    set(getFireRef('state', getState()), State.FOLDED)
-  }
+    set(getFireRef("state", getState()), State.FOLDED);
+  };
 }
 
-export function idle(){
+export function idle() {
   return (_, getState) => {
-    set(getFireRef('state', getState()), State.IDLE)
-  }
+    set(getFireRef("state", getState()), State.IDLE);
+  };
 }
 
-export function tie(){
+export function tie() {
   return (_, getState) => {
-    set(getFireRef('state', getState()), State.TIED)
-  }
+    set(getFireRef("state", getState()), State.TIED);
+  };
 }
 
 export function setPlayerHost() {
-  return { type: 'SET_PLAYER_HOST' }
+  return { type: "SET_PLAYER_HOST" };
 }
 
 // Watch table round update and execute a callback with the table
-function onRoundUpdate(table, cb){
-  let roundRef = `table/${table.id}/round`
+function onRoundUpdate(table, cb) {
+  let roundRef = `table/${table.id}/round`;
 
   // watch table round
-  const db = getDatabase()
-  onValue(ref(db, roundRef), async roundSnapshot => {
+  const db = getDatabase();
+  onValue(ref(db, roundRef), async (roundSnapshot) => {
     const playerSnapshot = await loadPlayers(table),
-          updatedTable = {
-            player: playerSnapshot.val(),
-            round: roundSnapshot.val()
-          }
+      updatedTable = {
+        player: playerSnapshot.val(),
+        round: roundSnapshot.val(),
+      };
 
-    cb(updatedTable)
-  })
+    cb(updatedTable);
+  });
 }
 
-function loadPlayers(table){
-  let playerRef = `table/${table.id}/player`
-  const db = getDatabase()
-  return get(ref(db, playerRef))
+function loadPlayers(table) {
+  let playerRef = `table/${table.id}/player`;
+  const db = getDatabase();
+  return get(ref(db, playerRef));
 }
--- src/features/ActionBar/components/ActionBar.js
+++ src/features/ActionBar/components/ActionBar.js
@@ -1,13 +1,55 @@
-import { Component } from 'react'
+import { Component } from "react";
+import { connect } from "react-redux";
+import { fold } from "actions/player-action";
+import selectors from "reducers/selectors";
+import State from "constants/state";
+
+const getState = (props) => {
+  const isAlone = props.players.length <= 1;
+  const isFolded = props.me.state === State.FOLDED;
+
+  if (isAlone) {
+    return "WAITING_FOR_PLAYERS";
+  }
+
+  if (isFolded) {
+    return "IS_FOLDED";
+  }
+
+  return "FOLD";
+};
 
 /**
  * Big buttons at the bottom of screen
  */
 class ActionBar extends Component {
   render() {
-    // If there's 0 opponents, show that we're waiting on more players
-    return <div>Bet/Fold/Call</div>
+    const { fold } = this.props;
+    const state = getState(this.props);
+
+    return (
+      <div>
+        {state === "WAITING_FOR_PLAYERS" && (
+          <button disabled>Waiting on more players</button>
+        )}
+        {state === "IS_FOLDED" && <button disabled>Folded</button>}
+        {state === "FOLD" && <button onClick={fold}>Fold</button>}
+      </div>
+    );
   }
 }
 
-export default ActionBar
+function mapStateToProps(state) {
+  return {
+    me: selectors.getPlayers(state)[0],
+    players: selectors.getPlayers(state),
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    fold: () => dispatch(fold()),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ActionBar);
