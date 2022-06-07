2a457fb2bd2ba67e56a13521f9e7e0fa3dc1f116
Ilias Deros <IliasDeros@users.noreply.github.com>
2022-06-07 15:46:43 +0000

    Add bets

--- src/features/Table/components/Player.js
+++ src/features/Table/components/Player.js
@@ -1,16 +1,25 @@
 import React from "react";
 import { PlayerAvatar } from "./PlayerAvatar";
-import { StyledPlayer } from "./styles";
+import {
+  StyledPlayer,
+  StyledPlayerBet,
+  StyledPlayerButton,
+  StyledPlayerName,
+  StyledPlayerStack,
+} from "./styles";
 
 export default Player = ({ avatarProps, index, player }) => {
   return (
     <StyledPlayer>
-      <div className="absolute">
-        {player.name} {player.isHost && "(host)"}
+      <div className="relative">
+        <PlayerAvatar index={index} avatarProps={avatarProps} />
+        <StyledPlayerName>
+          {player.name} {player.isHost && "(host)"}
+        </StyledPlayerName>
+        <StyledPlayerButton>{player.button}</StyledPlayerButton>
+        <StyledPlayerStack>{player.chips}</StyledPlayerStack>
+        {player.turnBet && <StyledPlayerBet>{player.turnBet}</StyledPlayerBet>}
       </div>
-      <PlayerAvatar index={index} avatarProps={avatarProps} />
-
-      <div className="absolute">{player.button}</div>
     </StyledPlayer>
   );
 };
--- src/features/Table/components/styles.js
+++ src/features/Table/components/styles.js
@@ -1,10 +1,25 @@
 import "./styles.css";
 
-// Reference: https://codepen.io/Rovak/pen/ExYeQar
 export const StyledTable = ({ children }) => (
   <div className="table">{children}</div>
 );
 
 export const StyledPlayer = ({ children }) => (
   <div className="table__player">{children}</div>
+);
+
+export const StyledPlayerName = ({ children }) => (
+  <div className="table__player-name">{children}</div>
+);
+
+export const StyledPlayerButton = ({ children }) => (
+  <div className="table__player-button">{children}</div>
+);
+
+export const StyledPlayerStack = ({ children }) => (
+  <div className="table__player-stack">{children}</div>
+);
+
+export const StyledPlayerBet = ({ children }) => (
+  <div className="table__player-bet">{children}</div>
 );
