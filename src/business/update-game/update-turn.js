35b8cc2835f9a0e1c5f961ccbe7592215c13eab3
Ilias Deros <IliasDeros@users.noreply.github.com>
2022-06-08 17:03:55 +0000

    Fix turn finish logic
    Resolve an issue where the game would
    unexpectedly skip to showdown

--- src/business/update-game/update-turn.js
+++ src/business/update-game/update-turn.js
@@ -1,31 +1,69 @@
-import isTurnFinished from "business/is-turn-finished";
 import State from "constants/state";
 import Turn from "constants/turn";
 import { compose, increment, _get, _update } from "./utils";
 
-const canBet = (p) => ![State.ALL_IN, State.FOLDED].includes(_get(p, "state"));
-const calledEveryoneAllIn = (players) => {
-  const isAlone = players.length === 1;
-  const bettingPlayers = players.filter(canBet);
-  const notEveryoneElseIsAllIn = bettingPlayers.length !== 1;
-  const isEveryoneAllIn = bettingPlayers.length === 0;
-
-  if (isEveryoneAllIn) {
-    return true;
-  }
-
-  if (isAlone || notEveryoneElseIsAllIn) {
-    return false;
-  }
-  const highestBet = players
-    .filter((p) => _get(p, "state") !== State.FOLDED)
-    .reduce((bet, p) => Math.max(bet, _get(p, "turnBet", 0)), 0);
-  const hasCalledHighestBet = _get(bettingPlayers[0], "turnBet") >= highestBet;
-
-  return hasCalledHighestBet;
+/**
+ * Can't proceed to next turn if we are at the last turn
+ */
+const stayShowdown = (data) => {
+  const { table } = data;
+  const isShowdownTurn = _get(table, "turn") === Turn.FINISHED;
+  return isShowdownTurn && data;
 };
-const isShowdown = (players, table) =>
-  _get(table, "turn") === Turn.FINISHED || calledEveryoneAllIn(players);
+
+const findHighestBet = (players) => {
+  const maxBet = (bet, p) => Math.max(bet, _get(p, "turnBet", 0));
+  return players.reduce(maxBet, 0);
+};
+const isAllIn = (p) => {
+  const chips = _get(p, "chips", 0);
+  const roundBet = _get(p, "roundBet", 0);
+  return chips <= 0 && roundBet > 0;
+};
+const isIdle = (p) => _get(p, "state") === State.IDLE;
+const isFolded = (p) => _get(p, "state") === State.FOLDED;
+const hasCalledHighestBet = (players) => {
+  const bet = findHighestBet(players);
+  return (p) => !isIdle(p) && _get(p, "turnBet", 0) >= bet;
+};
+
+/**
+ * Ignoring folded players:
+ *
+ * Proceed to next turn if everyone called the highest bet.
+ *
+ * Skip straight to the final turn if:
+ * - 0 or 1 player called the highest bet
+ * - 1+ people are all-in
+ */
+const goToNextTurn = (data) => {
+  const { players, table } = data;
+  const hasCalled = hasCalledHighestBet(players);
+  const activePlayers = players.filter((p) => !isFolded(p));
+  const isTurnFinished = activePlayers.every(hasCalled);
+  const isShowdown = activePlayers.filter((p) => !isAllIn(p)).length <= 1;
+  const nextTurn = isShowdown ? Turn.FINISHED : increment(table.turn);
+
+  if (!isTurnFinished && !isShowdown) {
+    return data;
+  }
+
+  const hasBet = (p) => _get(p, "turnBet", 0) > 0;
+  const clearBet = (p) => (hasBet(p) ? _update(p, { turnBet: 0 }) : p);
+  const resetActiveState = (p) =>
+    isFolded(p) ? p : _update(p, { state: State.IDLE });
+
+  return {
+    players: players.map(compose(resetActiveState, clearBet)),
+    table: _update(table, { turn: nextTurn }),
+  };
+};
+
+const stayAlone = (data) => {
+  const { players } = data;
+  const isAlone = players.length === 1;
+  return isAlone && data;
+};
 
 /**
  * Proceed to next turn when everyone played
@@ -41,30 +79,6 @@ const isShowdown = (players, table) =>
  * }
  * ```
  */
-export default function updateTurn({ players, table }) {
-  if (isShowdown(players, table)) {
-    return {
-      players,
-      table: _update(table, { turn: Turn.FINISHED }),
-    };
-  }
-
-  if (!isTurnFinished(players)) {
-    return { players, table };
-  }
-
-  const clearBet = (p) => _update(p, { turnBet: 0 });
-  const resetState = (p) => {
-    const cannotBet = [State.ALL_IN, State.FOLDED].includes(_get(p, "state"));
-    if (cannotBet) {
-      return p;
-    }
-
-    return _update(p, { state: State.IDLE });
-  };
-
-  return {
-    players: players.map(compose(resetState, clearBet)),
-    table: _update(table, { turn: increment(table.turn) }),
-  };
+export default function updateTurn(data) {
+  return stayAlone(data) || stayShowdown(data) || goToNextTurn(data);
 }
