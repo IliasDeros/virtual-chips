657ee50e451949c9a0e24fdeb07a544d996aa0fb
Ilias Deros <IliasDeros@users.noreply.github.com>
2022-06-14 20:24:09 +0000

    Fix tie

--- src/features/ActionBar/components/ActionsTied.js
+++ src/features/ActionBar/components/ActionsTied.js
@@ -1,6 +1,6 @@
 import { StyledAction } from "./styles";
 
-export const ActionsWaitingForPlayers = () => (
+export const ActionsTied = () => (
   <StyledAction action="tie" disabled>
     Tied
   </StyledAction>
